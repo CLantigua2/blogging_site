a934b6952685d08ea3c45d4a7451722ddf63873e
Carlos Lantigua <clantigua954@gmail.com>
2021-05-01 21:24:44 -0400

    added image for seo

--- gatsby-config.js
+++ gatsby-config.js
@@ -124,7 +124,7 @@ module.exports = {
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
         display: `standalone`,
-        icon: "src/images/me.jpg",
+        icon: "src/images/Carlos-Lantigua-midcrop-small.jpg",
       },
     },
   ],
